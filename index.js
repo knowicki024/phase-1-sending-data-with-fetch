f046e6cc1ef361f5115bdbaac161b3c91498b2b6
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-20 19:33:24 -0500

    lab homework

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+const submitData = {
+   name: userName,
+    email: userEmail
+};
+
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(submitData),
+})
+.then(response => response.json())
+.then(data => {
+    const id = data.id;
+    document.body.append(id);
+})
+.catch(error => {
+    document.body.append(error.message);
+})
+       
+}
